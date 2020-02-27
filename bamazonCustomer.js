b8d9750accfaba3b73ed3158da9818dee505e42f
Mangenese <blakemangen20@gmail.com>
2020-02-26 19:29:19 -0700

    added the connection query with it printing the table in node

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,29 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+    
+    user: "root",
+
+    password: "root",
+
+    database: "bamazon"
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId + "\n")
+    loadItems();
+});
+
+function loadItems() {
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+
+        console.table(res);
+
+        inquireCustomer(res)
+    })
+}
