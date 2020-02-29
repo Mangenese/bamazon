b1a0bb4bb6551d96ee12c669ad79e60b9c59856e
Mangenese <blakemangen20@gmail.com>
2020-02-28 21:19:21 -0700

    added psuedo code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
+//Require calls for mysql, inquirer, console.table
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
-
+//making the call to our SQL database
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -13,12 +14,13 @@ var connection = mysql.createConnection({
 
     database: "bamazon"
 });
+//on connect it shows the connection thread
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId + "\n")
     loadItems();
 });
-
+//on connection it pull from bamazon db and produces the table in the console.log
 function loadItems() {
     connection.query("SELECT * FROM products", function(err, res){
         if (err) throw err;
@@ -28,7 +30,7 @@ function loadItems() {
         inquireCustomerForItem(res)
     })
 }
-
+//function asking the user what ID they would like to buy
 function inquireCustomerForItem(quantity) {
     inquirer
         .prompt([
@@ -37,13 +39,14 @@ function inquireCustomerForItem(quantity) {
             name: "choice",
             message: "What is the ID of the item you would to buy?",
             validate: function(val){
+                //this function so the user has to input a number to proceed
                 return !isNaN(val)
             }
         }
     ]).then(function(val){
         var choice = parseInt(val.choice);
         var product = checkQunatity(choice, quantity);
-
+        //makes a check to see if the product you selected has enough in stock for what you want to buy
         if(product) {
             inquireCustomerForQuantity(product);
         }
@@ -53,6 +56,7 @@ function inquireCustomerForItem(quantity) {
         }
     })
 }
+//asking how much the cust wants to buy
 function inquireCustomerForQuantity(product) {
     inquirer
     .prompt([
@@ -61,6 +65,7 @@ function inquireCustomerForQuantity(product) {
             name: "quantity",
             message: "How many would you like?",
             validate: function(val) {
+                //user must return a value greater than 0
                 return val > 0
             }
         }
@@ -75,8 +80,9 @@ function inquireCustomerForQuantity(product) {
         }
     });
 }
-
+// this is the function to allow the user to buy any ID they want
 function makeTransaction(product, quantity) {
+    //setting a varianble so multiple of the same ID can be purchased with its amount
     var totalCost = product.price * quantity
     connection.query(
         
@@ -89,7 +95,7 @@ function makeTransaction(product, quantity) {
         }
     );
 }
-
+//This is checking to see if the ID item is in stock or not
 function checkQunatity(choice, quantity) {
     for (var i = 0; i <quantity.length; i++){
         if (quantity[i].item_id === choice) {
